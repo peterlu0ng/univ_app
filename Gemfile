9ddf38f21329bb7bf376d24fb62783c933ff6914
Peter Luong <peter.luong678@gmail.com>
2024-07-06 21:40:56 +1000

    add sidenav and top dropdown

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ gem "rubocop"
 gem "materialize-sass"
 gem 'material_icons'
 gem 'sassc-rails'
+gem "jquery-rails"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// alert("Inside 'application.js' file");
+
+$(document).ready(function() {
+    // alert("Inside 'document.ready()' handler");
+    $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+  });
