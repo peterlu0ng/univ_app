9d4dc169d8e4f253a02351f52799f2792c3c1166
Peter Luong <peter.luong678@gmail.com>
2024-07-07 20:29:36 +1000

    add students edit, show new pages

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,22 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: %i[show edit update]
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = 'You have successfully updated your profile'
+      redirect_to @student
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def new
@@ -10,7 +26,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(student_params)
     if @student.save
-      flash[:success] = 'You have successfully signed up'
+      flash[:notice] = 'You have successfully signed up'
       redirect_to root_path
     else
       render 'new'
@@ -22,5 +38,9 @@ class StudentsController < ApplicationController
 
   def student_params
     params.require(:student).permit(:name, :email)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
