1f4488d683c40e45f5d219cc80fe639e661f9af7
Peter Luong <peter.luong678@gmail.com>
2024-07-15 13:56:07 +1000

    add student courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_06_115053) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_051323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,11 +22,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_06_115053) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
